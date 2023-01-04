7d79db1623d34f67267313ff3af430ade0f3e7a5
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-01-04 15:06:32 +0200

    +-

--- src/services/api.js
+++ src/services/api.js
@@ -1,67 +1,44 @@
-// https://api.themoviedb.org/3/movie/550?api_key=4327815596da5129cea06268029b0666
-// const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const API_KEY = '4327815596da5129cea06268029b0666';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export default function fetchTrendingMovies(page) {
-  return axios.get(
-    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
+const api = {
+  findTrendingMovies,
+  findByKeyword,
+  findActors,
+  findDetails,
+  findReview,
+};
+
+const findTrendingMovies = async () => {
+  const response = await axios.get(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+  );
+  return response.data.results;
+};
+const findByKeyword = async (searchQuery, page) => {
+  const response = await axios.get(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`,
     {
       params: {
         page: `${page}`,
       },
     }
   );
-  
-}
+  return response.data;
+};
 
+const findDetails = async movieId => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data;
+};
 
-
-export default function findById(id) {
-    return axios
-    .get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`, {})
-    .catch(function (error) {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-    });
-  }
-
-
-export default function findByKeyword(searchQuery, page) {
-    return axios
-      .get(
-        `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`,
-        {
-          params: {
-            page: `${page}`,
-          },
-        }
-      )
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          console.log(error.request);
-        } else {
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-      });
-  }
-
-  fetchTrendingMovies.propTypes = {
-  page: PropTypes.number.isRequired,
+const findActors = async movieId => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data.cast;
 };
