48cdf10128513a618b400f0b71e9bcba5a0859a7
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-01-08 21:06:22 +0000

    Deploying to gh-pages from @ AnastasiiaSteblyna/goit-react-hw-05-movies@28bd102588eb4468d7b2cbba010bcfa74b211790 🚀

--- static/js/541.c600a8f5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[541],{541:function(e,i,n){n.r(i);var s=n(184);i.default=function(){return(0,s.jsx)("main",{children:(0,s.jsx)("h1",{children:"Movies"})})}}}]);
-//# sourceMappingURL=541.c600a8f5.chunk.js.map
--- static/js/845.ebfc0670.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[845],{845:function(e,i,n){n.r(i);var s=n(184);i.default=function(){return(0,s.jsx)("main",{children:(0,s.jsx)("h1",{children:"Movies"})})}}}]);
-//# sourceMappingURL=845.ebfc0670.chunk.js.map
