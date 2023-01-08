28bd102588eb4468d7b2cbba010bcfa74b211790
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-01-08 23:05:20 +0200

    +

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,8 +1,49 @@
+import Loader from 'components/Loader/Loader';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from 'services/api';
+import css from '../../styles/Common.module.css';
+
 const Cast = () => {
+  const { movieId } = useParams();
+  const [actors, setActors] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const onActors = async () => {
+      setLoading(true);
+      try {
+        const actors = await api.findActors(movieId);
+        console.log(actors);
+        setActors(actors);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    onActors();
+  }, [movieId]);
+
   return (
-    <main>
-      <h1>Movies</h1>
-    </main>
+    <div>
+      {loading && <Loader />}
+      {actors && (
+        <ul className={css.list}>
+          {actors.map(actor => (
+            <li className={css.cast} key={actor.id}>
+              <img
+                width="100px"
+                src={'https://image.tmdb.org/t/p/w500' + actor.profile_path}
+                alt={actor.original_name}
+              />
+              <p>{actor.name}</p>
+              <p>Character: {actor.character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,45 @@
-const Review = () => {
+import Loader from 'components/Loader/Loader';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from 'services/api';
+import css from '../../styles/Common.module.css';
+
+const Cast = () => {
+  const { movieId } = useParams();
+  const [review, setReview] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const onReview = async () => {
+      setLoading(true);
+      try {
+        const review = await api.findReview(movieId);
+        console.log(review);
+        setReview(review);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    onReview();
+  }, [movieId]);
+
   return (
-    <main>
-      <h1>Movies</h1>
-    </main>
+    <div>
+      {loading && <Loader />}
+      {review && (
+        <ul className={css.list}>
+          {review.map(r => (
+            <li className={css.list} key={r.id}>
+              <h3>Author: {r.author}</h3>
+              <p>{r.content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
-export default Review;
+export default Cast;
